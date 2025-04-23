b5ec1b7a6ee2d01704b23e867cdb00ef3aa2dd70
riicardojs <ricardo.jeronymo@hotmail.com>
2025-04-23 00:09:57 -0300

    feat: basic configuration project

---
+++ src/infra/db/schemas/uploads.ts
@@ -0,0 +1,12 @@
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { randomUUID } from 'node:crypto'
+
+export const uploads = pgTable('uploads', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => randomUUID()),
+  name: text('name').notNull(),
+  remoteKey: text('remote_key').notNull().unique(),
+  remoteUrl: text('remote_url').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+})
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,53 @@
 import fastifyCors from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import fastify from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImagesRoute } from './routes/upload-images'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      error: 'Validation Error',
+      issues: error.validation,
+    })
+  }
+
+  // Envie o erro para alguma ferramenta de observalidade (Sentry/Datadog/Grafana/Otel)
+
+  console.log(error)
+  reply.status(500).send({ error: 'Internal Server Error' })
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Uplaod Server',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.register(uploadImagesRoute)
 
 server
   .listen({
